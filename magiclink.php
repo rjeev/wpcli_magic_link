642d1270f15d38dfe0e89e60a5cb448a01cd6aa5
vijars <obsession.raj@gmail.com>
2023-05-14 18:28:31 +0545

    Changes for composer according to repo

--- magiclink.php
+++ magiclink.php
@@ -1,46 +1,31 @@
 <?php
 
-
-// if (!defined('WPODB_PLUGIN_MAIN_PATH')) define('WPODB_PLUGIN_MAIN_PATH', __DIR__);
-// if (!defined('WPODB_VERSION')) define('WPODB_VERSION', '1.0.0');
-
 class WP_ActivateMagicLogin extends WP_CLI_Command
 {
 
-    public function __construct()
-    {
-        $this->enablePlugin();
-    }
-
+    
     public function enablePlugin()
     {
 
-        WP_CLI::runcommand('plugin install ./magic_link.zip');
-        // WP_CLI::success("Magic link plugin activated");
-    }
-}
+        $path = WP_CLI::runcommand('package path', array('return'=>true, 'error'=>false));
+        $package_path = $path.'vendor/rjeev/wpcli_magic_link/magic_link.zip';
+        WP_CLI::runcommand('plugin install '.$package_path);
+        WP_CLI::runcommand('plugin activate magic_link');
+        WP_CLI::success("Magic link plugin activated");    
+        
 
-
-class WP_DeActivateMagicLogin extends WP_CLI_Command
-{
-
-    public function __construct()
-    {
-        $this->disablePlugin();
+        
     }
 
     public function disablePlugin()
     {
-        $dir_path = plugin_dir_path(__FILE__);
-
-        mkdir($dir_path . '/magic_link/');
-        copy('magic_link.php', $dir_path . '/magiclink/magic_link.php');
-
-        WP_CLI::runcommand('plugin deactivate Magic Link Login');
-        mkdir($dir_path . '/magic_link/');
+        
+        WP_CLI::runcommand('plugin deactivate magic_link');
+        WP_CLI::runcommand('plugin delete magic_link');
         WP_CLI::success("Magic link plugin removed");
     }
 }
 
-WP_CLI::add_command('activateMagicLogin', 'WP_ActivateMagicLogin');
-WP_CLI::add_command('deactivateMagicLogin', 'WP_DeActivateMagicLogin');
+
+
+WP_CLI::add_command('magicLogin', 'WP_ActivateMagicLogin');
